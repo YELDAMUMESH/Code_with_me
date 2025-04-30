7a34e1608e1b155ccc74e57550e4d17ba321ad3a
YELDAMUMESH <gliscor68@gmail.com>
2025-04-30 09:05:11 +0530

    Added length of string

---
+++ chapter_10/Strings/length_of_string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    char *ptr = str;
+    int count=0;
+    while(*ptr != '\0')
+    {
+        count++;
+        ptr++;
+    }
+    printf("Length of the string is: %d\n", count);
+
+return 0;
+}
