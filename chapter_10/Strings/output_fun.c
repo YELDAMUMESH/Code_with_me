18258e6c3803e3df40572042eb8e1bc88e16a891
YELDAMUMESH <gliscor68@gmail.com>
2025-04-30 15:52:36 +0530

    Modified output function

--- chapter_10/Strings/output_fun.c
+++ chapter_10/Strings/output_fun.c
@@ -1,13 +1,29 @@
-#include<stdio.h>
+
+#include <stdio.h>
+
 int main()
 {
-
+    char str[100];
     char ch;
-    for(ch='A';ch<='Z';ch++)
+    int i=0;
+    printf("Enter the string: ");
+    while((ch=getchar())!='\n' && i<sizeof(str)-1)
     {
-        putchar(ch);
-        printf(" ");
+        str[i]=ch;
+        i++;
+    }
+    str[i]='\0';
+    
+    char *p;
+    p=str;
+    
+    while(*p!='\0')
+    {
+        putchar(*p);
+        p++;
+        
     }
     printf("\n");
-return 0;
+
+    return 0;
 }
